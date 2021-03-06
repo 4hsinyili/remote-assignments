05d0051ecfd022ec05c7d16ef7c405daac798e1c
4hsinyili <4hsinyili@gmail.com>
2021-03-06 18:59:51 +0800

    update

--- Week-2/Assignment-5/week_2_assignment_5_last.py
+++ Week-2/Assignment-5/week_2_assignment_5_last.py
@@ -64,7 +64,3 @@ def binary_search_last(numbers, target):
         return inner_search(mid, high)
     elif target < numbers[mid]:
         return inner_search(0, mid)
-
-print(binary_search_last([1, 2, 5, 5, 5, 6, 7], 2)) # should print 1 (the index of the target number ‘2’) 
-print(binary_search_last([1, 2, 5, 5, 5, 6, 7], 5)) # should print 2 (the index of the ‘first’ target number ‘5’ shows up) 
-print(binary_search_last([1, 2, 5, 5, 5, 6, 7], 3)) # should print 2 (since it can’t find number 3, return the index of ‘the smallest number larger then 3', that is, the index of the ‘first’ number 5)
